484f1c58fd2c85351a9609f042fa57eb94da8755
Adarkai <164913979+Adarkai@users.noreply.github.com>
2024-08-04 05:25:27 +0700

    Added: trang chu, sach, tai khoan
    no functionality yet

---
+++ DangKy.designer.cs
@@ -0,0 +1,236 @@
+﻿using System.Windows.Forms;
+using System;
+
+namespace WindowsFormsApp2
+{
+    partial class FormDangky
+    {
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void dangkybtn_Click(object sender, EventArgs e)
+        {
+            // Display success message
+            
+        }
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dangkybtn = new System.Windows.Forms.Button();
+            this.hotendntxt = new System.Windows.Forms.TextBox();
+            this.emailtxt = new System.Windows.Forms.TextBox();
+            this.tendnltxt = new System.Windows.Forms.TextBox();
+            this.sdttxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.passtxt = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.retrypasstxt = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(141, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đăng ký tài khoản";
+            // 
+            // dangkybtn
+            // 
+            this.dangkybtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dangkybtn.Location = new System.Drawing.Point(136, 484);
+            this.dangkybtn.Name = "dangkybtn";
+            this.dangkybtn.Size = new System.Drawing.Size(94, 36);
+            this.dangkybtn.TabIndex = 1;
+            this.dangkybtn.Text = "Đăng ký";
+            this.dangkybtn.UseVisualStyleBackColor = true;
+            this.dangkybtn.Click += new System.EventHandler(this.dangkybtn_Click_1);
+            // 
+            // hotendntxt
+            // 
+            this.hotendntxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hotendntxt.Location = new System.Drawing.Point(145, 106);
+            this.hotendntxt.Multiline = true;
+            this.hotendntxt.Name = "hotendntxt";
+            this.hotendntxt.Size = new System.Drawing.Size(262, 34);
+            this.hotendntxt.TabIndex = 2;
+            // 
+            // emailtxt
+            // 
+            this.emailtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailtxt.Location = new System.Drawing.Point(145, 229);
+            this.emailtxt.Multiline = true;
+            this.emailtxt.Name = "emailtxt";
+            this.emailtxt.Size = new System.Drawing.Size(262, 34);
+            this.emailtxt.TabIndex = 3;
+            // 
+            // tendnltxt
+            // 
+            this.tendnltxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tendnltxt.Location = new System.Drawing.Point(145, 167);
+            this.tendnltxt.Multiline = true;
+            this.tendnltxt.Name = "tendnltxt";
+            this.tendnltxt.Size = new System.Drawing.Size(262, 34);
+            this.tendnltxt.TabIndex = 4;
+            // 
+            // sdttxt
+            // 
+            this.sdttxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sdttxt.Location = new System.Drawing.Point(145, 287);
+            this.sdttxt.Multiline = true;
+            this.sdttxt.Name = "sdttxt";
+            this.sdttxt.Size = new System.Drawing.Size(262, 34);
+            this.sdttxt.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Họ tên";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Tên đăng nhập";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 227);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 288);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "SĐT";
+            // 
+            // passtxt
+            // 
+            this.passtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passtxt.Location = new System.Drawing.Point(145, 349);
+            this.passtxt.Multiline = true;
+            this.passtxt.Name = "passtxt";
+            this.passtxt.Size = new System.Drawing.Size(262, 34);
+            this.passtxt.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 352);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Mật khẩu";
+            // 
+            // retrypasstxt
+            // 
+            this.retrypasstxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.retrypasstxt.Location = new System.Drawing.Point(145, 407);
+            this.retrypasstxt.Multiline = true;
+            this.retrypasstxt.Name = "retrypasstxt";
+            this.retrypasstxt.Size = new System.Drawing.Size(262, 34);
+            this.retrypasstxt.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 410);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(131, 16);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Nhập lại mật khẩu";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // FormDangky
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(452, 561);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.retrypasstxt);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.passtxt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.sdttxt);
+            this.Controls.Add(this.tendnltxt);
+            this.Controls.Add(this.emailtxt);
+            this.Controls.Add(this.hotendntxt);
+            this.Controls.Add(this.dangkybtn);
+            this.Controls.Add(this.label1);
+            this.Name = "FormDangky";
+            this.Text = "Đăng ký";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button dangkybtn;
+        private System.Windows.Forms.TextBox hotendntxt;
+        private System.Windows.Forms.TextBox emailtxt;
+        private System.Windows.Forms.TextBox tendnltxt;
+        private System.Windows.Forms.TextBox sdttxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private TextBox passtxt;
+        private Label label6;
+        private TextBox retrypasstxt;
+        private Label label7;
+    }
+}
